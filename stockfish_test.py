8d094c5bcb611468ad762d47ae636ec2b759e5f9
John <nesnidal@iastate.edu>
2025-02-22 15:35:27 -0600

    Modified variable name

--- stockfish_test.py
+++ stockfish_test.py
@@ -12,11 +12,11 @@ import stockfish
 fish = stockfish.Stockfish()
 
 # Set this variable to True for white's pov, False for black's POV
-pov_white = False
+POV_WHITE = False
 
 print(fish.get_parameters())
 print()
 print("BOARD CONFIGURATION (start of game, from white's POV)")
 print("White is represented as UPPERCASE")
 print("Black is represented as lowercase")
-print(fish.get_board_visual(pov_white)) # True for white's POV, False for black's POV
+print(fish.get_board_visual(POV_WHITE)) # True for white's POV, False for black's POV
