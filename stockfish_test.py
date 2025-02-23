12d1b3ad9e90a13969467250affba9be886ec565
John <nesnidal@iastate.edu>
2025-02-22 18:07:23 -0600

    Demonstrated the chess engine moving the pieces

--- stockfish_test.py
+++ stockfish_test.py
@@ -20,3 +20,15 @@ print("BOARD CONFIGURATION (start of game, from white's POV)")
 print("White is represented as UPPERCASE")
 print("Black is represented as lowercase")
 print(fish.get_board_visual(POV_WHITE))
+
+# White moves once
+move_w1 = fish.get_best_move()
+print(move_w1)
+fish.make_moves_from_current_position([move_w1])
+print(fish.get_board_visual(POV_WHITE))
+
+# Black moves once
+move_b1 = fish.get_best_move()
+print(move_b1)
+fish.make_moves_from_current_position([move_b1])
+print(fish.get_board_visual(POV_WHITE))
